b4be70168bc7933095f5914f9ab9078373fd2a63
DmitryZhV <zhirovdv@gmail.com>
2019-04-05 17:13:23 +0300

    Edit init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+mkdir /home/box/web/public
+mkdir /home/box/web/public/img
+mkdir /home/box/web/public/css
+mkdir /home/box/web/public/js
+mkdir /home/box/web/uploads
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
