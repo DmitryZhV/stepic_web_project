3d78f8bc577d1bd0ca7e5c34e1632e2eec0e3083
DmitryZhV <zhirovdv@gmail.com>
2019-04-07 21:47:05 +0300

    new version' ;

--- hello.py
+++ hello.py
@@ -1,11 +1,22 @@
+#import multiprocessing
+
+bind = "0.0.0.0:8080"
+#workers = multiprocessing.cpu_count() * 2 + 1
 
 def app(environ, start_response):
-        data =[(i+'\n') for i in environ['QUERY_STRING'].split('&')]
+        data = [bytes(i+'\n') for i in environ['QUERY_STRING'].split('&')]
+        print(len(data))
+        print(len([data]))
         s=''
         for i in data:
             s+=i
+        print(s)
+
+        
+        
         start_response("200 OK", [
             ("Content-Type", "text/plain"),
             ("Content-Length", str(len(s)))
         ])
+        
         return s
--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo pip install --upgrade django
 sudo /etc/init.d/gunicorn stop
 sudo /etc/init.d/nginx restart
 mkdir /home/box/web/public
@@ -7,6 +8,7 @@ mkdir /home/box/web/public/img
 mkdir /home/box/web/public/css
 mkdir /home/box/web/public/js
 mkdir /home/box/web/uploads
-#sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+#sudo ln -s /home/box/web/etc/gunicorn.py   /etc/gunicorn.d/gunicorn.py
 sudo gunicorn -b 0.0.0.0:8080 hello:app 
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application
 #sudo /etc/init.d/mysql start
